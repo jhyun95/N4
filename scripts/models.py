89a42ffea29593ef6135d3ce7b471b4543c462ac
Jason Hyun <jhyun95@gmail.com>
2018-04-09 04:04:24 -0700

    Playing with tanh-linear layers

---
+++ scripts/models.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  9 03:13:28 2018
+
+@author: jhyun_000
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+
+def transform_default():
+    return [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+
+def transform_list():
+    return [transforms.ToTensor(), 
+            lambda x: x > 0.5,
+            lambda x: x.float()]
+    
+class LinNet(nn.Module):
+    def __init__(self):
+        super(LinNet, self).__init__()
+        self.lin1 = nn.Linear(784,112)
+        self.lin2 = nn.Linear(112,16)
+        self.lin3 = nn.Linear(16,10)
+#        self.lin4 = nn.Linear(4,1)
+        
+    def forward(self, x):
+        x = x.view(-1,784)
+        x = F.tanh(self.lin1(x))
+        x = F.tanh(self.lin2(x))
+        x = F.log_softmax(self.lin3(x))
+#        x = F.tanh(self.lin4(x))
+        return x
+
+class BaseNet(nn.Module):
+    def __init__(self):
+        super(BaseNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+class StnNet(nn.Module):
+    # From http://pytorch.org/tutorials/intermediate/spatial_transformer_tutorial.html?highlight=mnist
+    def __init__(self):
+        super(StnNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+        # Spatial transformer localization-network
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(10 * 3 * 3, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+
+        # Initialize the weights/bias with identity transformation
+        self.fc_loc[2].weight.data.fill_(0)
+        self.fc_loc[2].bias.data = torch.FloatTensor([1, 0, 0, 0, 1, 0])
+
+    # Spatial transformer network forward function
+    def stn(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 10 * 3 * 3)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
+
+    def forward(self, x):
+        # transform the input
+        x = self.stn(x)
+
+        # Perform the usual forward pass
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
--- scripts/test.py
+++ scripts/test.py
@@ -7,14 +7,14 @@ Created on Fri Apr  6 20:52:54 2018
 
 from __future__ import print_function
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
-from mkl import set_num_threads
+import models
 
-set_num_threads(4)
+#from mkl import set_num_threads
+#set_num_threads(4)
 
 def main():
     # Training settings   
@@ -23,8 +23,8 @@ def main():
     TEST_BATCH = 1000
     LEARNING_RATE = 0.01
     MOMENTUM = 0.5
-    EPOCHS = 10
-    LOG_INTERVAL = 10   
+    EPOCHS = 1
+    LOG_INTERVAL = 25  
 #    args = parser.parse_args()
 #    print(args)
 #    torch.manual_seed(args.seed)  
@@ -35,46 +35,23 @@ def main():
     #kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
     kwargs = {}
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+        datasets.MNIST('../data', train=True, download=True, 
+                       transform=transforms.Compose(models.transform_list())),
         batch_size=INPUT_BATCH, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+        datasets.MNIST('../data', train=False, download=True, 
+                       transform=transforms.Compose(models.transform_list())),
         batch_size=TEST_BATCH, shuffle=True, **kwargs)
     
     ''' Model training epochs '''
-    model = Net()
+    model = models.LinNet()
 #    if CUDA:
 #        model.cuda()
     optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
     for epoch in range(1, EPOCHS + 1):
         train(model, train_loader, optimizer, epoch, LOG_INTERVAL)
         test(model, test_loader)
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
-
+        
 def train(model, train_loader, optimizer, epoch, log_interval):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -90,7 +67,6 @@ def train(model, train_loader, optimizer, epoch, log_interval):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
-            sys.stdout.flush()
 
 def test(model, test_loader):
     model.eval()
@@ -109,7 +85,6 @@ def test(model, test_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-    sys.stdout.flush()
 
 if __name__ == '__main__':
     main()
