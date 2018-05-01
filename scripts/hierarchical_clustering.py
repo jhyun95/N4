Fixed MCC formula, ontology extraction from hierarchical clustering + visualization

scripts/hierarchical_clustering.py
@@ -1,82 +1,109 @@
-''' From pyNexo_Fast.py, by majianzhu '''
+''' Adapted from pyNexo_Fast.py, by majianzhu '''
 
-import numpy as np
 import time
-from scipy.cluster import hierarchy
+import numpy as np
+import scipy.cluster.hierarchy as sch
+import scipy.spatial.distance as ssd
 from scipy.stats import mannwhitneyu
 
-def hierarchial_clustering(D, threshold, min_size, ncpus):
-	#Here D should be a numpy matrix on CPU
-	ngene, _ = D.shape
-	time1 = time.time()
-	
-	Z = hierarchy.linkage( D[np.triu_indices(ngene,1)], 'average' )
-	Z = Z.astype(int)
-	
-	time2 = time.time()
-	print('Hierarchical clustering takes', time2 - time1,'seconds')
+import matplotlib.pyplot as plt
+import networkx as nx
+# For viz, needs pygraphviz
+# https://stackoverflow.com/questions/40528048/pip-install-pygraphviz-no-package-libcgraph-found
 
-	nmerge = Z.shape[0]
-	nterm = nmerge + ngene
-	
-	global adj
-	adj = np.zeros((nterm, nterm))
-	global assoc
-	assoc = np.zeros((nterm, ngene))
-	assoc[0:ngene, 0:ngene] = np.eye(ngene)
-	pvalue_list = np.zeros((nterm,))
+def hierarchial_clustering(D, p_threshold, min_cluster_size, 
+                           plot_dendrogram=False,
+                           plot_ontology=False):
+    ngene, _ = D.shape
+    time1 = time.time()
+    
+    ''' Hierarchical clustering via SciPy '''
+    condensed = ssd.squareform(D)
+    Z = sch.linkage(condensed, 'average')
+    if plot_dendrogram:
+        fig1, ax1 = plt.subplots()
+        sch.dendrogram(Z, ax=ax1, no_labels=True)
+    Z = Z.astype(int)
+    time2 = time.time()
+    print('Hierarchical clustering takes', time2 - time1,'seconds')
 
-	for i in range(nmerge):
+    ''' Merge clusters based on p-values from Mann-Whitney U test '''
+    nmerge = Z.shape[0]
+    nterm = nmerge + ngene
+    adj = np.zeros((nterm, nterm))
+    assoc = np.zeros((nterm, ngene))
+    assoc[0:ngene, 0:ngene] = np.eye(ngene)
+    pvalue_list = np.zeros((nterm,))
 
-		C = [ assoc[ Z[i,0], :], assoc[ Z[i,1], :] ]
+    for i in range(nmerge):
+        C = [ assoc[ Z[i,0], :], assoc[ Z[i,1], :] ]
+        adj[ ngene+i, Z[i,0] ] = 1 # cluster ngene+i has two children Z[i,0] and Z[i,1]
+        adj[ ngene+i, Z[i,1] ] = 1
+        assoc[ ngene+i, :] = np.logical_or(C[0] , C[1]) # genes of ngene+i = genes of Z[i,0] + genes of Z[i,1]
+        for child in range(2):
+            if Z[i,child] >= ngene:
+                k = Z[i,child] - ngene  #parent term k index
+                A1 = assoc[ Z[k,0], :].nonzero()[0] #child1 of term k
+                B = assoc[ Z[k,1], :].nonzero()[0]  #child2 of term k
+                CC = C[1-child].nonzero()[0]
+                A_B_interaction = D[A1,:][:,B].flatten()
+                A_C_interaction = D[A1,:][:,CC].flatten()
+                B_C_interaction = D[B,:][:,CC].flatten()
+                AC_BC_interaction = np.concatenate((A_C_interaction, B_C_interaction), axis=0)
+                
+                ''' Edit: Address elements that are all identical, which is 
+                    not handled by native mannwhitneyu, i.e. testing 
+                    [1,1] vs [1,1,1]. Assign pvalue = 1.0 for these cases '''
+                try:
+                    pvalue = mannwhitneyu(A_B_interaction, AC_BC_interaction, alternative='less')[1]
+                except ValueError:
+                    pvalue = 1.0
+                pvalue_list[ Z[i,child] ] = pvalue
 
-		adj[ ngene+i, Z[i,0] ] = 1 # cluster ngene+i has two children Z[i,0] and Z[i,1]
-		adj[ ngene+i, Z[i,1] ] = 1
-		assoc[ ngene+i, :] = np.logical_or(C[0] , C[1]) # genes of ngene+i = genes of Z[i,0] + genes of Z[i,1]
-		
-		for child in range(2):
-			if Z[i,child] >= ngene:
-				
-				k = Z[i,child] - ngene  #parent term k index
+    time3 = time.time()
+    print('Calculate pvalue takes', time3 - time2,'seconds')
 
-				A1 = assoc[ Z[k,0], :].nonzero()[0] #child1 of term k
-				B = assoc[ Z[k,1], :].nonzero()[0]  #child2 of term k
-				CC = C[1-child].nonzero()[0]
+    ''' Prune ontology based on p-value threshold '''
+    to_remove = pvalue_list > p_threshold
+    keep = pvalue_list <= p_threshold
+    nterm = np.sum(keep)
+    assoc = assoc[keep, :]
+    for i in to_remove.nonzero()[0]:
+        for p in (adj[:,i]==1).nonzero()[0]:
+            adj[p,:] = np.logical_or(adj[p,:], adj[i,:])
+        adj[i,:] = 0
+        adj[:,i] = 0
 
-				A_B_interaction = D[A1,:][:,B].flatten()
-				A_C_interaction = D[A1,:][:,CC].flatten()
-				B_C_interaction = D[B,:][:,CC].flatten()
+    adj = adj[keep,:][:,keep]
+    filt = np.sum(assoc, axis=1) >= min_cluster_size
+    assoc = assoc[filt,:]
+    adj = adj[filt,:][:,filt]
+    nterm = assoc.shape[0]
+    time4 = time.time()
+    print('Prune ontology takes', time4 - time3,'seconds')
+    print('Cluster adjacencies:', adj.shape)
+    print('Cluster associations:', assoc.shape)
+    
+    ''' Visualize ontology '''
+    if plot_ontology:
+        nclust, _ = adj.shape
+        G = nx.from_numpy_matrix(adj)
+        clust_sizes = np.sum(assoc, 1)
+        labels = {}
+        for i in range(nclust):
+            labels[i] = str(int(clust_sizes[i]))
+        try: # if pygraphviz is available
+            layout = nx.drawing.nx_agraph.graphviz_layout(G, prog='dot')
+            nx.draw(G, pos=layout, arrows=True, node_size=400, 
+                    with_labels=True, labels=labels)    
+        except ImportError:
+            print('No pygraphviz, using spring layout')
+            nx.draw_spring(G, arrows=True, node_size=400, with_labels=True, labels=labels)    
+        
+    return adj, assoc, Z
 
-				AC_BC_interaction = np.concatenate((A_C_interaction, B_C_interaction), axis=0)
-				pvalue_list[ Z[i,child] ] = mannwhitneyu(A_B_interaction, AC_BC_interaction, alternative='less')[1]
+data_file = '../data/pixel_correlations/pixel_mcc_adj_3s.csv.gz'
+corr = np.loadtxt(data_file, delimiter=',')
+distances = np.max(corr) - np.abs(corr)
 
-	time3 = time.time()
-	print('Calculate pvalue takes', time3 - time2,'seconds')
-
-	to_remove = pvalue_list > threshold
-	keep = pvalue_list <= threshold
-	nterm = np.sum(keep)
-
-	assoc = assoc[keep, :]
-
-	for i in to_remove.nonzero()[0]:
-		for p in (adj[:,i]==1).nonzero()[0]:
-			adj[p,:] = np.logical_or(adj[p,:], adj[i,:])
-
-		adj[i,:] = 0
-		adj[:,i] = 0
-
-	adj = adj[keep,:][:,keep]
-
-	filt = np.sum(assoc, axis=1) >= min_size
-
-	assoc = assoc[filt,:]
-	adj = adj[filt,:][:,filt]
-	nterm = assoc.shape[0]
-	
-	time4 = time.time()
-	
-	print('Prune ontology takes', time4 - time3,'seconds')
-	print(adj.shape)
-	print(assoc.shape)
-	#return adj, assoc, Z
+adj, assoc, Z = hierarchial_clustering(distances, 0.05, 2, False, True)

scripts/interactions.py
@@ -29,7 +29,9 @@ def main():
 #    find_lethal_pixels()
 #    find_2nd_order_interactions()
     
-    compute_pixel_correlations_parallel(mode='mcc-adj')
+    compute_pixel_correlations_parallel(sets=range(10), mode='mcc', cpus=2)
+    compute_pixel_correlations_parallel(sets=range(10), mode='mcc-adj', cpus=2)
+#    find_pixel_correlations(mode='mcc')
     
 #    heatmap_correlation(CORR_DIR + 'pixel_sokal_michener_0s.csv.gz'); plt.figure()
 #    heatmap_correlation(CORR_DIR + 'pixel_mcc_0s.csv.gz'); plt.figure()
@@ -88,7 +90,7 @@ def get_pixel(p):
     return (p % DIM, int((p-p%DIM)/DIM))
 
 def find_pixel_correlations(model=MODEL, labelset=2, check_consistency=True,
-                            mode='sokal-michener', output_file='pixel_corr.csv'):
+                            mode='sokal-michener', output_file=None):
     ''' Create pairwise pixel correlations for either all images, or for 
         a particular label (i.e. 2s only). Uses model-generated labels, 
         not actual labels. Checks for consistency by default, i.e. 
@@ -122,8 +124,7 @@ def find_pixel_correlations(model=MODEL, labelset=2, check_consistency=True,
     print('Loaded', N, 'images.')
     images = torch.stack(images)
     
-    ''' Compute pairwise pixel correlations for black and white images
-        NOTE: Simply takes the 2*(# times pixels equal) / (# images) - 1'''    
+    ''' Compute pairwise pixel correlations for black and white images '''    
     correlations = np.zeros(shape=(DIM*DIM,DIM*DIM))
     for p1 in range(DIM*DIM):
         print('Image Set:', labelset, 'Pixel #:', p1+1, 'of', DIM*DIM)        
@@ -144,14 +145,18 @@ def find_pixel_correlations(model=MODEL, labelset=2, check_consistency=True,
                 d = N - a - b - c # both black
                 if mode == 'mcc-adj': # pseudocounts
                     a += 1; b += 1; c += 1; d += 1
-                numer = (a+d) - (b+c)
+                numer = (a*d) - (b*c)
                 denom = (a+b)*(a+c)*(b+d)*(c+d)
-                corr = numer/denom**0.5 if denom > 0 else 0.0
+                corr = (numer*numer/denom)**0.5 if denom > 0 else 0.0
+                if np.abs(corr) > 1.0: # rounding error?
+                    print('WARNING:',corr,a,b,c,d)
             correlations[p1,p2] = corr
             correlations[p2,p1] = corr
     
-    np.savetxt(output_file, correlations, delimiter=',', newline='\n')
+    if output_file != None:
+        np.savetxt(output_file, correlations, delimiter=',', newline='\n')
     print('Time (seconds):', round(time.time() - start_time, 3))
+    return correlations
 
 def find_2nd_order_interactions(model=MODEL, first_order_file=OUTPUT_1ST,
                                 output_file = OUTPUT_2ND):